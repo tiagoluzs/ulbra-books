ed2baa9939e6decb630d526d64f9feeed76c1445
Tiago Luz <tiago.luz@tolv12.com>
2019-10-07 21:04:04 -0300

    fix

--- ulbrabooks.js
+++ ulbrabooks.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var request = require('request');
+
 var htmlparser = require("htmlparser");
 
 var handler = new htmlparser.DefaultHandler(function (error, dom) {
@@ -41,7 +42,7 @@ var parseUlbraHtml = function(body) {
       let author = tableResult.children[i].children[5].children[0].raw.trim();
       let title = tableResult.children[i].children[7].children[0].raw.trim();
       let publisher = tableResult.children[i].children[9].children[0].raw.trim();
-      if(publisher == "BR") { 
+      if(publisher == "BR") {
         publisher = null;
       }
       let year = tableResult.children[i].children[11].children[0].raw.trim();
@@ -66,6 +67,7 @@ app.get('/', function (req, res) {
 
 app.get('/busca', function (req, res) {
   let query = req.query['query'] || null;
+  let page = req.query['page'] || null;
 
   if(query == null) {
     res.json({
@@ -74,9 +76,8 @@ app.get('/busca', function (req, res) {
     });
   } else {
 
-
     let url = "https://servicos.ulbra.br/ALEPH?";
-    url += "func=find-b&request="+query+"&find_code=WRD&local_base=ULB01&x=0&y=0&filter_code_4=WMA&filter_request_4=&filter_code_1=WLN&filter_request_1=&filter_code_2=WYR&filter_request_2=&filter_code_3=WYR&filter_request_3=";
+    url += func + "&request="+query+"&find_code=WRD&local_base=ULB01&x=0&y=0&filter_code_4=WMA&filter_request_4=&filter_code_1=WLN&filter_request_1=&filter_code_2=WYR&filter_request_2=&filter_code_3=WYR&filter_request_3=";
 
     request(url, function (error, response, body) {
       if(error) {
@@ -85,9 +86,7 @@ app.get('/busca', function (req, res) {
           errormsg: error
         });
       } else {
-
         res.json(parseUlbraHtml(body));
-
       }
     });
 
